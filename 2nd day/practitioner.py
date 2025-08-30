4f36644da78d28a03947f93370d0a603b5773730
AndreOliv421 <andreoliv421@gmail.com>
2025-08-30 11:47:01 -0300

    add

---
+++ 2nd day/practitioner.py
@@ -0,0 +1,11 @@
+print("Bem vindo ao Sistema")
+print("Digite se vcoê é maior de idade")
+valor = float(input ("Digite 1 para maior 0 para menor"))
+
+if valor == 1:
+        print("Você é maior de idade")
+elif valor == 0:
+        print("Você é menor de idade")
+else:
+        print("é uma opção invalida")
+        
