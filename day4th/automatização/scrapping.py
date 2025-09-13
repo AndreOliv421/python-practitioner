12a46c21786df6a2eaae26fdf10e5310c56168c1
AndreOliv421 <andreoliv421@gmail.com>
2025-09-13 12:49:38 -0300

    up

--- day4th/automatização/scrapping.py
+++ day4th/automatização/scrapping.py
@@ -10,7 +10,7 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) Applewebkit/537.36"
 }
 
-url = "https://br.tradingview.com/symbols/BTCUSD/news/?exchange=BITSTAMP"
+url = "https://g1.globo.com/"
 
 # Fazendo Requisição http
 resposta = requests.get(url, headers=headers)
@@ -22,7 +22,7 @@ if resposta.status_code == 200:
 
     soup = BeautifulSoup(resposta.text, "html.parser")
 
-    noticias = soup.find_all("a", class_="apply-overflow-tooltip apply-overflow-tooltip--direction_both block-bETdSLzM title-BpSwpmE_ title-DmjQR0Aa")
+    noticias = soup.find_all("a", class_="feed-post-link")
     
     print("Ultimas noticias:")
     for noticia in noticias:
